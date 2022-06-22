db94a9c13c5476e8440fa2eeeae0bad97a29c376
benjaminharuna1 <105396700+benjaminharuna1@users.noreply.github.com>
2022-06-22 16:44:06 +0000

    added changes

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-let name = prompt("What is your name")
+let name = prompt("What is your name");
 
-let height = prompt("What is your height")
+let height = prompt("What is your height");
 
-let country = prompt("What is your country")
+let country = prompt("What is your country");
 
-alert(`your name is ${name}, you are ${height} in height and you are from ${country}`)
+alert(`your name is ${name}, you are ${height} in height and you are from ${country}`);
